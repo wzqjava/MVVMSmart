9566fd1349a4f199f22e131d2533166ee1ae9902
wzq <316301354@qq.com>
2020-01-09 22:55:04 +0800

    添加BaseRecyclerViewAdapterHelper

--- app/src/main/java/com/wzq/sample/ui/rv_multi/MultiRecycleViewFragment.java
+++ app/src/main/java/com/wzq/sample/ui/rv_multi/MultiRecycleViewFragment.java
@@ -2,13 +2,15 @@ package com.wzq.sample.ui.rv_multi;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.listener.OnItemChildClickListener;
 import com.wzq.mvvmsmart.base.BaseFragment;
-import com.wzq.mvvmsmart.rv_adapter.BaseViewAdapter;
-import com.wzq.mvvmsmart.rv_adapter.BindingViewHolder;
-import com.wzq.mvvmsmart.rv_adapter.MultiTypeAdapter;
 import com.wzq.mvvmsmart.utils.ToastUtils;
 import com.wzq.sample.R;
 import com.wzq.sample.bean.DemoBean;
@@ -17,9 +19,6 @@ import com.wzq.sample.utils.TestUtils;
 
 import java.util.ArrayList;
 
-import androidx.annotation.Nullable;
-import androidx.recyclerview.widget.LinearLayoutManager;
-
 
 /**
  * Create Date：2019/01/25
@@ -27,10 +26,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
  */
 
 public class MultiRecycleViewFragment extends BaseFragment<FragmentMultiRvBinding, MultiRecycleViewModel> {
-    private static final int VIEW_TYPE_ONE = 1;
-    private static final int VIEW_TYPE_TWO = 2;
-    private MultiTypeAdapter mMultiTypeAdapter;
-    private static final ArrayList<DemoBean.ItemsEntity> list = new ArrayList<>();
+
+
+    private ArrayList<DemoBean.ItemsEntity> datas = new ArrayList<>();
+    private MyMultiAdapter mAdapter;
 
     @Override
     public int initContentView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -46,55 +45,49 @@ public class MultiRecycleViewFragment extends BaseFragment<FragmentMultiRvBindin
     public void initData() {
         super.initData();
         getData();
-        mMultiTypeAdapter = new MultiTypeAdapter(getActivity());
-        mMultiTypeAdapter.setPresenter(new DemoAdapterPresenter());
-        mMultiTypeAdapter.addViewTypeToLayoutMap(VIEW_TYPE_ONE, R.layout.item_multiple1);
-        mMultiTypeAdapter.addViewTypeToLayoutMap(VIEW_TYPE_TWO, R.layout.item_multiple2);
-        binding.setLayoutManager(new LinearLayoutManager(getActivity()));
+        initRecyclerView();
 
-        mMultiTypeAdapter.addAll(list, new MultiTypeAdapter.MultiViewTyper() {
+    }
+
+    private void initRecyclerView() {
+        mAdapter = new MyMultiAdapter(datas);
+        binding.setAdapter(mAdapter);
+        binding.setLayoutManager(new LinearLayoutManager(getActivity()));
+        binding.rv.addOnItemTouchListener(new OnItemChildClickListener() {
 
             @Override
-            public int getViewType(Object object) {
-                DemoBean.ItemsEntity item = object instanceof DemoBean.ItemsEntity ? ((DemoBean.ItemsEntity) object) : null;
-                if (item.getId() % 2 == 0) {
-                    return VIEW_TYPE_TWO;
-                } else {
-                    return VIEW_TYPE_ONE;
-                }
+            public void onSimpleItemChildClick(BaseQuickAdapter adapter, View view, int position) {
+
             }
         });
+        mAdapter.setOnItemChildClickListener(new BaseQuickAdapter.OnItemChildClickListener() {
+            @Override
+            public void onItemChildClick(BaseQuickAdapter adapter, View view, int position) {
+                DemoBean.ItemsEntity item = (DemoBean.ItemsEntity)adapter.getItem(position);
+                switch (view.getId()) {
+                    case R.id.btn1:
+                        ToastUtils.showShort("btn1" + item.getName());
+                        break;
+                    case R.id.btn2:
+                        ToastUtils.showShort("btn2" + item.getName());
 
-        binding.setAdapter(mMultiTypeAdapter);
-        mMultiTypeAdapter.setPresenter(new DemoAdapterPresenter());
+                        break;
+                }
+
+            }
+        });
     }
 
     private void getData() {
         for (int i = 0; i < 50; i++) {
-            list.add(new DemoBean.ItemsEntity(i, "MVVMSmart", TestUtils.GetGirlImgUrl()));
+            DemoBean.ItemsEntity ItemBean = new DemoBean.ItemsEntity(i, "MVVMSmart", TestUtils.GetGirlImgUrl());
+            if (i % 2 == 0) {
+                ItemBean.itemType = 0;
+            } else {
+                ItemBean.itemType = 1;
+            }
+            datas.add(ItemBean);
         }
     }
 
-    public class DemoAdapterPresenter implements BaseViewAdapter.Presenter {
-        public void onItemClick(DemoBean.ItemsEntity itemsEntity) {
-            Toast.makeText(getActivity(), "itemsEntity " + itemsEntity.getName(), Toast.LENGTH_SHORT).show();
-
-        }
-
-        public void onItemLongClick(DemoBean.ItemsEntity itemsEntity) {
-            Toast.makeText(getActivity(), "itemsEntity " + itemsEntity.getName(), Toast.LENGTH_SHORT).show();
-        }
-
-        public void onViewClick(DemoBean.ItemsEntity itemsEntity) {
-            ToastUtils.showShort("点击了button按钮");
-        }
-
-    }
-
-    public class DemoAdapterDecorator implements BaseViewAdapter.Decorator {
-        @Override
-        public void decorator(BindingViewHolder holder, int position, int viewType) {
-            // you may do something according to position or view type
-        }
-    }
 }
